a01beaa2fef440a8d6b1cd342dcaa5f84248742b
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-11-01 12:13:07 -0500

    Add navbar to top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopNavbar from './TopNavbar';
 
 function App() {
   return (
     <div className="App">
+      <TopNavbar></TopNavbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
